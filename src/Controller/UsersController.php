567386b6bbf9a861c5343a326d5b6c1f52939c8b
Floris <fli-fo10@hotmail.fr>
2021-05-05 21:06:56 +0200

    add authentication

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,6 +11,13 @@ namespace App\Controller;
  */
 class UsersController extends AppController
 {
+
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->allowUnauthenticated(['login']);
+    }
     /**
      * Index method
      *
@@ -124,5 +131,24 @@ class UsersController extends AppController
             );
         }
         $this->set(compact('geoArray'));
+    }
+
+    public function login()
+    {
+        $result = $this->Authentication->getResult();
+        // If the user is logged in send them away.
+        if ($result->isValid()) {
+            $target = $this->Authentication->getLoginRedirect() ?? '/home';
+            return $this->redirect($target);
+        }
+        if ($this->request->is('post') && !$result->isValid()) {
+            $this->Flash->error('Invalid username or password');
+        }
+    }
+
+    public function logout()
+    {
+        $this->Authentication->logout();
+        return $this->redirect(['controller' => 'Users', 'action' => 'login']);
     }
 }
